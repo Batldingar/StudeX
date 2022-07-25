f48bbbc7c69bd68eb3ec80d9ac8ee9338c9ee58d
Batldingar <baldinger.andreas@gmail.com>
2022-07-25 11:30:43 +0200

    Added clickable pdf pages

--- app/src/main/java/at/hagenberg/studex/ui/PDFView.kt
+++ app/src/main/java/at/hagenberg/studex/ui/PDFView.kt
@@ -1,13 +1,14 @@
 package at.hagenberg.studex.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,6 +29,7 @@ import at.hagenberg.studex.R
  * @param navHostController The navigation host controller
  * @param viewModel A pdf view model (populated automatically)
  */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PDFView(
     pdfID: Int?,
@@ -37,6 +39,7 @@ fun PDFView(
     val context = LocalContext.current
     val bitmapList: ArrayList<ImageBitmap>? by viewModel.bitmapList.observeAsState()
     val progress: Float? by viewModel.loadingProgress.observeAsState()
+    val selectedCardList = remember { mutableStateListOf<Int>() }
 
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
 
@@ -49,14 +52,26 @@ fun PDFView(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 bitmapList?.let { it1 ->
-                    itemsIndexed(items = it1) { _, bitmap ->
-                        Image(
-                            modifier = Modifier.fillMaxWidth(),
-                            bitmap = bitmap,
-                            alignment = Alignment.Center,
-                            contentScale = ContentScale.FillWidth,
-                            contentDescription = "The current PDF page"
-                        )
+                    itemsIndexed(items = it1) { index, bitmap ->
+                        Card(
+                            onClick = {
+                                if (!selectedCardList.contains(index)) selectedCardList.add(
+                                    index
+                                ) else selectedCardList.remove(index)
+                            },
+                            modifier = if (selectedCardList.contains(index)) Modifier
+                                .fillMaxWidth()
+                                .border(2.dp, colorResource(id = R.color.button_view))
+                            else Modifier.fillMaxWidth()
+                        ) {
+                            Image(
+                                bitmap = bitmap,
+                                modifier = Modifier.fillMaxWidth(),
+                                alignment = Alignment.Center,
+                                contentScale = ContentScale.FillWidth,
+                                contentDescription = "The current PDF page"
+                            )
+                        }
                     }
                 }
             }
